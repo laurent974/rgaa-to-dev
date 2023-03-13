f51d58871e5df2d150f95d35732991ae313b67cc
Laurent <lyenpon974@gmail.com>
2023-03-13 19:45:26 +0100

    Init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+const StylelintPlugin = require("stylelint-webpack-plugin"); // line to add
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
-};
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
+  },
+}
+
+module.exports = nextConfig
