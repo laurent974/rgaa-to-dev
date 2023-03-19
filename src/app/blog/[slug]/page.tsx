dc774821fa47e6b8914deb035e1a5066cbc5018b
Laurent <lyenpon974@gmail.com>
2023-03-19 20:30:05 +0100

    Update - Structure project

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,39 +1,5 @@
-
-import { promises as fs } from 'fs'
-import { serialize } from 'next-mdx-remote/serialize'
-import { type MDXRemoteSerializeResult } from 'next-mdx-remote';
-import { MdxContent } from '@/components/MdxContent';
-import path from 'path';
-
-type Frontmatter = {
-  title: string;
-  date: string;
-}
-
-type Post<TFrontmatter> = {
-  serialized: MDXRemoteSerializeResult;
-  frontmatter: TFrontmatter;
-}
-
-const getPostContent: (slug: string) => Promise<Post<Frontmatter>> = async (slug: string): Promise<Post<Frontmatter>> => {
-  const folderSrc = path.resolve(process.cwd(), "src");
-  const folder = `${folderSrc}/posts/`
-  const file = `${folder}${slug}.mdx`
-  const content: string = await fs.readFile(file, 'utf-8')
-
-  // Serialize the MDX content and parse the frontmatter
-  const serialized: MDXRemoteSerializeResult<Record<string, unknown>, Record<string, unknown>> = await serialize(content, {
-    parseFrontmatter: true,
-  });
-
-  // Typecast the frontmatter to the correct type
-  const frontmatter = serialized.frontmatter as Frontmatter;
-
-  return {
-    frontmatter,
-    serialized
-  }
-}
+import { MdxContent } from '@/components/mdx/MdxContent';
+import getPostContent from '@/lib/api/get-post-content';
 
 const BlogPage:(props: any) => Promise<JSX.Element> = async (props: any): Promise<JSX.Element> => {
   const slug: string = props.params.slug
---
+++ src/lib/types/post.tsx
@@ -0,0 +1,8 @@
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
+
+type Post<TFrontmatter> = {
+  serialized: MDXRemoteSerializeResult;
+  frontmatter: TFrontmatter;
+}
+
+export default Post
