904294cb2682bbaea84d7402ff1305de9a974e7e
Laurent <lyenpon974@gmail.com>
2023-03-25 01:19:19 +0100

    Add - Menu

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,58 +1,72 @@
+import { NextPage } from 'next'
 import fs from 'fs'
-import { globby } from "globby"
-import matter from "gray-matter"
-import Link from "next/link"
-import path from "path"
+import path from 'path'
 import styles from './Nav.module.scss'
 
-const getPostData = async () => {
-  const listAllFilesAndDirs = (dir: string) => globby(`${dir}/**/*`);
-  let posts: any = []
-
-  const files = await listAllFilesAndDirs(path.resolve(process.cwd(), "src/posts"));
-
-  files.map((file: string) => {
-    const markdownWithMeta: string = fs.readFileSync(file, 'utf-8')
-    const { data: frontMatter } = matter(markdownWithMeta)
-
-    posts.push({
-      frontMatter,
-      slug: file.replace(/.*(?=posts)/gm, '').replace('posts/', '').replace('/index', '').split('.')[0]
-    })
-  })
-
-  return posts
+type MenuItem = {
+  title: string
+  link: string
+  children?: MenuItem[]
 }
 
-export const Nav: any = async () => {
-  const posts = await getPostData()
+type MenuProps = {
+  menuData: MenuItem[]
+}
+
+const buildMenu = (parentFolder: string, currentFolder: string): MenuItem => {
+  const currentPath = path.join(parentFolder, currentFolder)
+  const items = fs.readdirSync(currentPath)
+    .filter(name => name.endsWith('.mdx'))
+    .map(name => {
+      return {
+        title: name.replace('.mdx', ''),
+        link: path.join('/', currentFolder, name.replace('.mdx', ''))
+      }
+    })
+  const children = fs.readdirSync(currentPath)
+    .filter(name => fs.lstatSync(path.join(currentPath, name)).isDirectory())
+    .map(name => buildMenu(currentPath, name))
+  if (children.length) {
+    return {
+      title: currentFolder,
+      link: path.join('/', currentFolder),
+      children: [
+        ...items,
+        ...children
+      ]
+    }
+  }
+
+  return {
+    title: currentFolder,
+    link: path.join('/', currentFolder),
+    children: items
+  }
+}
+
+const buildMenuData = (rootFolder: string): MenuItem[] => {
+  return fs.readdirSync(rootFolder)
+    .filter(name => fs.lstatSync(path.join(rootFolder, name)).isDirectory())
+    .map(name => buildMenu(rootFolder, name))
+}
+
+export const Nav: NextPage<MenuProps> = () => {
+  const menuData = buildMenuData(path.join(process.cwd(), 'src', 'posts'))
+
+  const renderMenu = (menuItems: MenuItem[]) => (
+    <ul className={styles.list}>
+      {menuItems.filter((item) => item.link.indexOf('index') < 0).map((item, index) => (
+        <li key={index}>
+          <a href={item.link}>{item.title}</a>
+          {item.children && renderMenu(item.children)}
+        </li>
+      ))}
+    </ul>
+  )
 
   return (
-    <nav aria-label="Menu principal" className={ styles.nav }>
-      <ul>
-        <li><Link href="/">Accueil</Link></li>
-
-        {
-          // collections.map((collection: Collection, index: number): JSX.Element => (
-          //   <li className="has-submenu" key={index}>
-          //     <Link href="/" aria-expanded="false">{ collection.title }</Link>
-          //     <button aria-expanded="false">
-          //       <span className="visually-hidden">Ouvre le sous-menu de { collection.title }</span>
-          //     </button>
-
-              <ul>
-                {posts.map((post: any, index: number): JSX.Element => (
-                  <li key={ index }>
-                    <Link href={ `/${ post.frontMatter.collection }/` + post.slug } passHref key={index}>
-                      { post.frontMatter.title }
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-          //   {/*</li>
-          // )) */}
-        }
-      </ul>
+    <nav className={ styles.nav }>
+      { renderMenu(menuData) }
     </nav>
   )
 }
