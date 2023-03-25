dbf3be2519e62c7b8d6beed0e414e3eaaf3aca70
Laurent <lyenpon974@gmail.com>
2023-03-25 02:07:36 +0100

    Add - Logic to show articles

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,12 +1,14 @@
 import { MdxContent } from '@/components/mdx/MdxContent';
 import getPostContent from '@/lib/api/get-post-content';
 
-const BlogPage:(props: any) => Promise<JSX.Element> = async (props: any): Promise<JSX.Element> => {
-  const slug: string = props.params.slug
-  const { frontmatter, serialized } = await getPostContent(slug)
+const BlogPage:(props: any) => Promise<JSX.Element> = async (props) => {
+  const arrayToPath: any = (arr: any[]) => arr.join('/')
+  const path = arrayToPath(props.params.slug)
+  const { frontmatter, serialized } = await getPostContent(path)
 
   return (
     <div>
+      <h1>parametres</h1>
       <h1>{frontmatter.title}</h1>
       <p>Published {frontmatter.date}</p>
       <MdxContent source={ serialized }/>
