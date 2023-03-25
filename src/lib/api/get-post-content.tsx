4d6a610970ef7cf001ac00da68e513ff93f6ecf9
Laurent <lyenpon974@gmail.com>
2023-03-25 16:21:26 +0100

    Update - Path for menu

--- src/lib/api/get-post-content.tsx
+++ src/lib/api/get-post-content.tsx
@@ -1,27 +1,44 @@
-import { type MDXRemoteSerializeResult } from 'next-mdx-remote';
-import Frontmatter from '../types/Frontmatter';
-import Post from '../types/Post';
-import path from "path";
+import { type MDXRemoteSerializeResult } from 'next-mdx-remote'
+import Frontmatter from '../types/Frontmatter'
+import Post from '../types/Post'
+import path from 'path'
 import { serialize } from 'next-mdx-remote/serialize'
 import { promises as fs } from 'fs'
+import * as oldfs from 'fs'
 
-const getPostContent: (slug: string) => Promise<Post<Frontmatter>> = async (slug: string): Promise<Post<Frontmatter>> => {
-  const folderSrc: string = path.resolve(process.cwd(), "src");
+const getPostContent: (slug: string) => Promise<Post<Frontmatter>> = async (
+  slug: string
+): Promise<Post<Frontmatter>> => {
+  const folderSrc: string = path.resolve(process.cwd(), 'src')
   const folder = `${folderSrc}/posts/`
   const file = `${folder}${slug}.mdx`
-  const content: string = await fs.readFile(file, 'utf-8')
+  let content: string
+
+  try {
+    await fs.access(
+      file,
+      oldfs.constants.F_OK | oldfs.constants.W_OK | oldfs.constants.R_OK
+    )
+    content = await fs.readFile(file, 'utf-8')
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+  } catch (error: any) {
+    content = await fs.readFile(`${folder}${slug}/index.mdx`, 'utf-8')
+  }
 
   // Serialize the MDX content and parse the frontmatter
-  const serialized: MDXRemoteSerializeResult<Record<string, unknown>, Record<string, unknown>> = await serialize(content, {
+  const serialized: MDXRemoteSerializeResult<
+    Record<string, unknown>,
+    Record<string, unknown>
+  > = await serialize(content, {
     parseFrontmatter: true,
-  });
+  })
 
   // Typecast the frontmatter to the correct type
-  const frontmatter = serialized.frontmatter as Frontmatter;
+  const frontmatter = serialized.frontmatter as Frontmatter
 
   return {
     frontmatter,
-    serialized
+    serialized,
   }
 }
 
